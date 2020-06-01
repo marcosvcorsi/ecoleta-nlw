a689d341299b770036bdf2bcc97b9e6d3b62ff83
Marcos Corsi <marcosvcorsi@gmail.com>
2020-06-01 20:14:06 -0300

    basic setup API

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-  return response.json({ ok: true });
+  return response.json(['Marcos', 'Diego', 'Daniel']);
 });
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
