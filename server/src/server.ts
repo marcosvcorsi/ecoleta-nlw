e7eae289678ba495fb75d335108a0b6581313fbe
Marcos Corsi <marcosvcorsi@gmail.com>
2020-06-02 20:18:06 -0300

    typeorm added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata';
+
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json(['Marcos', 'Diego', 'Daniel']);
-});
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
