ed942bffbeb62ef4e3478a681a94c20ed1e80320
Marcos Corsi <marcosvcorsi@gmail.com>
2020-06-03 21:39:27 -0300

    pages created

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes';
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return <Routes />;
+};
 
 export default App;
