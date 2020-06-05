f2e3faa60ccb143b1594f7821cc4c3e9314d16c9
Marcos Corsi <marcosvcorsi@gmail.com>
2020-06-04 22:02:27 -0300

    IP changed

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -12,7 +12,7 @@ export default class ItemsController {
     const serializedItems = items.map((item) => {
       return {
         ...item,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.0.100:3333/uploads/${item.image}`,
       };
     });
 
