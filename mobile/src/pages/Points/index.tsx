de6e141293426ee34b3a8fd080b96cce1be1b9e4
Marcos Corsi <marcosvcorsi@gmail.com>
2020-06-05 21:53:56 -0300

    app finished

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -11,7 +11,7 @@ import {
   SafeAreaView,
   Alert,
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
 import * as Location from 'expo-location';
@@ -31,8 +31,16 @@ interface Point {
   longitude: number;
 }
 
+interface Params {
+  uf: string;
+  city: string;
+}
+
 const Points = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
 
   const [points, setPoints] = useState<Point[]>([]);
   const [items, setItems] = useState<Item[]>([]);
@@ -75,15 +83,15 @@ const Points = () => {
     api
       .get('/points', {
         params: {
-          city: 'Francisco Beltrão',
-          uf: 'PR',
-          items: [1, 2],
+          city: routeParams.city,
+          uf: routeParams.uf,
+          items: selectedItems,
         },
       })
       .then((response) => {
         setPoints(response.data);
       });
-  }, []);
+  }, [selectedItems]);
 
   const handleNavigateBack = useCallback(() => {
     navigation.goBack();
